9ce1316ec818464c4e66545a71b7d972f31b14d1
Jhang512 <a10947863@hotmail.com>
2019-07-10 01:03:58 +0800

    final version

--- guess-num.py
+++ guess-num.py
@@ -1,14 +1,30 @@
 import random
 
-r = random.randint(1,100)
+start = input('Enter a start number: ')
+end = input('Enter a end number: ')
+
+start = int(start)
+end = int(end)
+
+r = random.randint(start,end)
+
+count = 0
 
 while True:
+
+	count += 1 # count = count + 1
+	
 	num = input('Make a guess:')
-	num = int(num)
-	if r == num:
-		print('You are correct!')
-		break
-	else if num > r:
-		print('bigger than r')
-	else if num < r:
-		print('smaller than r')
+	if num == '':
+		print('You must input something')
+	else:
+		num = int(num)
+		if r == num:
+			print('You are correct!')
+			print('This is your', count, 'attempt')
+			break
+		elif num > r:
+			print('bigger than r')
+		elif num < r:
+			print('smaller than r')
+		print('This is your', count, 'attempt')
